2a01481d400883cc3b8afd412f031ffb8c661a64
Gonzalo Barrazueta <u20191a972@upc.edu.pe>
2022-06-26 01:26:37 -0500

    Agregué datos de estudiantes
    Agregué función para cargar datos de estudiantes

---
+++ Scripts MongoDB/Students.sql
@@ -0,0 +1,64 @@
+db.Students.insertMany([{
+    FirstName: "Yoshinori",
+    LastName: "Kanemoto",
+    DNI: "71200517",
+    ClassId: ObjectId("ca4bbc7a2dd94ee581623c39"),
+    ParentId: ObjectId("78f6fa2df35c7fbdbaed8c29")
+},{
+    FirstName: "Jae",
+    MiddleName: "Min",
+    LastName: "Na",
+    DNI: "71200517",
+    ClassId: ObjectId("ca4bbc7a2dd94ee581623c40"),
+    ParentId: ObjectId("78f6fa2df35c7fbdbaed8c30")
+},{
+    FirstName: "Miyeon",
+    LastName: "Choi",
+    DNI: "72480137",
+    ClassId: ObjectId("ca4bbc7a2dd94ee581623c41"),
+    ParentId: ObjectId("78f6fa2df35c7fbdbaed8c31")
+},{
+    FirstName: "Yeon",
+    MiddleName: "So",
+    LastName: "Jeon",
+    DNI: "55501234",
+    ClassId: ObjectId("ca4bbc7a2dd94ee581623c42"),
+    ParentId: ObjectId("78f6fa2df35c7fbdbaed8c32")
+},{
+    FirstName: "Felix",
+    LastName: "Lee",
+    DNI: "73091270",
+    ClassId: ObjectId("ca4bbc7a2dd94ee581623c43"),
+    ParentId: ObjectId("78f6fa2df35c7fbdbaed8c33")
+},{
+    FirstName: "Seung",
+    MiddleName: "Min",
+    LastName: "Kim",
+    DNI: "73033215",
+    ClassId: ObjectId("ca4bbc7a2dd94ee581623c44"),
+    ParentId: ObjectId("78f6fa2df35c7fbdbaed8c34")
+},{
+    FirstName: "Alessandra",
+    LastName: "Martinez",
+    DNI: "27803941",
+    ClassId: ObjectId("ca4bbc7a2dd94ee581623c45"),
+    ParentId: ObjectId("78f6fa2df35c7fbdbaed8c33")
+},{
+    FirstName: "Reggie",
+    LastName: "Sanchez",
+    DNI: "23131273",
+    ClassId: ObjectId("ca4bbc7a2dd94ee581623c40"),
+    ParentId: ObjectId("78f6fa2df35c7fbdbaed8c35")
+},{
+    FirstName: "Mateo",
+    LastName: "Fernandez",
+    DNI: "80321567",
+    ClassId: ObjectId("ca4bbc7a2dd94ee581623c40"),
+    ParentId: ObjectId("78f6fa2df35c7fbdbaed8c36")
+},{
+    FirstName: "Sana",
+    LastName: "Benavides",
+    DNI: "27890345",
+    ClassId: ObjectId("ca4bbc7a2dd94ee581623c39"),
+    ParentId: ObjectId("78f6fa2df35c7fbdbaed8c37")
+}]);
